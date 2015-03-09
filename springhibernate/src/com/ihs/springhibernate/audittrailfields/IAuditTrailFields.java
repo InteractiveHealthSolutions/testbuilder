68154848d95f87b6a4c9d55b199a891c9a12bf4a
saad.qureshi <saad.qureshi@ihsinformatics.com>
2015-03-09 09:04:30 +0500

    Removed commented code

--- springhibernate/src/com/ihs/springhibernate/audittrailfields/IAuditTrailFields.java
+++ springhibernate/src/com/ihs/springhibernate/audittrailfields/IAuditTrailFields.java
@@ -4,23 +4,9 @@ import java.util.Date;
 
 public interface IAuditTrailFields
 {
-	// public Date getCreatedDate();
-	//
-	// public void setCreatedDate(Date createdDate);
-	//
-	// public Date getLastUpdated();
-	//
-	// public void setLastUpdated(Date lastUpdatedDate);
-
 	public Date getCreationTS();
-
-
 	public void setCreationTS(Date creationTS);
 
-
 	public Date getLastEditedTS();
-
 	public void setLastEditedTS(Date lastEditedTS);
-
-
 }
