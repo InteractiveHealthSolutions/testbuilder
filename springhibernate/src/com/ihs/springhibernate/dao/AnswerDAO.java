b79a14633f0f53f714838a2304aeac511e880686
saad.qureshi <saad.qureshi@ihsinformatics.com>
2015-04-20 15:08:31 +0500

    New added classes for Answer, AnswerDate, and AnswerDAO

---
+++ springhibernate/src/com/ihs/springhibernate/dao/AnswerDAO.java
@@ -0,0 +1,288 @@
+package com.ihs.springhibernate.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.ihs.springhibernate.dao.QuestionDAO.By;
+import com.ihs.springhibernate.dao.QuestionDAO.FetchType;
+import com.ihs.springhibernate.formwrapper.FormTakingTest;
+import com.ihs.springhibernate.formwrapper.QuestionAnswer;
+import com.ihs.springhibernate.model.Answer;
+import com.ihs.springhibernate.model.AnswerData;
+import com.ihs.springhibernate.model.Question;
+import com.ihs.springhibernate.model.QuestionData;
+import com.ihs.springhibernate.utility.SessionFactoryBuilder;
+
+public class AnswerDAO
+{
+	static Logger log = Logger.getLogger(AnswerDAO.class);
+
+	public enum By
+	{
+		ID
+	}
+
+	public enum FetchType
+	{
+		LAZY, EAGER;
+	}
+
+	public static List<Answer> getAllAnswer(FetchType fetchType)
+	{
+		List<Answer> answerList = null;
+		Session session = null;
+
+		try
+		{
+			session = SessionFactoryBuilder.getSessionFactory().openSession();
+
+			session.beginTransaction();
+
+			String hql = "FROM Answer";
+			Query query = session.createQuery(hql);
+
+			answerList = (List<Answer>) query.list();
+
+			if (answerList != null)
+			{
+				if (fetchType == FetchType.EAGER)
+				{
+					for (Answer _answer : answerList)
+					{
+						Hibernate.initialize(_answer.getAnswerDataList());
+					}
+				}
+			}
+
+			session.getTransaction().commit();
+
+		}
+
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+
+		finally
+		{
+			if (session.isOpen())
+			{
+				session.close();
+			}
+		}
+
+		return answerList;
+	}
+
+	public static Answer getAnswer(By by, String value, FetchType fetchType)
+	{
+		Answer answer = null;
+		Boolean useInteger = false;
+
+		Session session = null;
+		try
+		{
+			session = SessionFactoryBuilder.getSessionFactory().openSession();
+			session.beginTransaction();
+
+			String sql = "FROM Answer WHERE ";
+
+			if (by.toString().equalsIgnoreCase(By.ID.toString()))
+			{
+				sql += "id= :_value";
+				useInteger = true;
+			}
+
+			// else if (by.toString().equalsIgnoreCase(By.TYPE_NAME.toString()))
+			// {
+			// sql += "type_name= :_value";
+			// }
+
+			Query query = session.createQuery(sql);
+
+			if (useInteger)
+			{
+				query.setParameter("_value", Integer.valueOf(value));
+			}
+
+			else
+			{
+				query.setParameter("_value", value.toLowerCase());
+			}
+
+			answer = (Answer) query.uniqueResult();
+
+			if (answer != null)
+			{
+				if (fetchType == FetchType.EAGER)
+				{
+					Hibernate.initialize(answer.getAnswerDataList());
+				}
+			}
+
+			session.getTransaction().commit();
+		}
+
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+
+		finally
+		{
+			if (session.isOpen())
+			{
+				session.close();
+			}
+		}
+
+		return answer;
+	}
+
+	public static Integer save(Answer answer)
+	{
+		Session session = null;
+
+		Integer newlySavedId = -1;
+
+		try
+		{
+			if (answer.getAnswerDataList().size() > 0)
+			{
+				for (AnswerData answerData : answer.getAnswerDataList())
+				{
+					answerData.setAnswer(answer);
+				}
+			}
+
+			session = SessionFactoryBuilder.getSessionFactory().openSession();
+
+			session.beginTransaction();
+			session.persist(answer);
+			session.getTransaction().commit();
+			newlySavedId = answer.getId();
+		}
+
+		catch (Exception ex)
+		{
+			Transaction tx = session.getTransaction();
+			tx.rollback();
+			ex.printStackTrace();
+		}
+
+		finally
+		{
+			if (session.isOpen())
+			{
+				session.close();
+			}
+		}
+
+		return newlySavedId;
+	}
+
+	public static List<Integer> save(List<Answer> answerList)
+	{
+		Session session = null;
+
+		List<Integer> newlySavedIdList = new ArrayList<Integer>();
+
+		try
+		{
+			for (Answer answer : answerList)
+			{
+				for (AnswerData answerData : answer.getAnswerDataList())
+				{
+					answerData.setAnswer(answer);
+				}
+			}
+
+			session = SessionFactoryBuilder.getSessionFactory().openSession();
+
+			Transaction tx = session.beginTransaction();
+
+			for (int i = 0; i < answerList.size(); i++)
+			{
+				session.save(answerList.get(i));
+
+				if (i % 20 == 0)
+				{
+					// 20, same as the JDBC batch size
+					// flush a batch of inserts and release memory:
+					session.flush();
+					session.clear();
+				}
+			}
+
+			tx.commit();
+			session.close();
+
+			// newlySavedId = answer.getId();
+		}
+
+		catch (Exception ex)
+		{
+			Transaction tx = session.getTransaction();
+			tx.rollback();
+			ex.printStackTrace();
+
+			log.error("Exception occured !", ex);
+		}
+
+		finally
+		{
+			if (session.isOpen())
+			{
+				session.close();
+			}
+		}
+
+		return newlySavedIdList;
+	}
+
+	/**
+	 * Removes null AnswerData from Answer
+	 * 
+	 * @param newAnswer
+	 * @return
+	 */
+	public static Answer removeNullAnswerData(Answer newAnswer)
+	{
+		String emprtString = "";
+		for (Iterator<AnswerData> itr = newAnswer.getAnswerDataList().iterator(); itr.hasNext();)
+		{
+			// if AnswerData is null or it is left empty then it should be removed
+			String dataToBeChecked = itr.next().getData();
+			if (dataToBeChecked == null || emprtString.equalsIgnoreCase(dataToBeChecked))
+			{
+				itr.remove();
+			}
+		}
+
+		return newAnswer;
+	}
+
+	/**
+	 * Removes null AnswerData from AnswerList of Exam
+	 * 
+	 * @param exam
+	 * @return
+	 */
+	public static FormTakingTest removeNullAnswerDataFromExam(FormTakingTest exam)
+	{
+		for (int i = 0; i < exam.getQuestionAnswerList().size(); i++)
+		{
+			Answer cleanAnswer = AnswerDAO.removeNullAnswerData(exam.getQuestionAnswerList().get(i).getAnswer());
+			exam.getQuestionAnswerList().get(i).setAnswer(cleanAnswer);
+		}
+
+		return exam;
+	}
+}
--- springhibernate/src/com/ihs/springhibernate/model/Answer.java
+++ springhibernate/src/com/ihs/springhibernate/model/Answer.java
@@ -1,18 +1,24 @@
 package com.ihs.springhibernate.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import com.ihs.springhibernate.audittrailfields.IAuditTrailFields;
 
@@ -26,30 +32,148 @@ public class Answer implements IAuditTrailFields
 	
 	@NotNull
 	@OneToOne
-	@JoinColumn(name = "question_id")
-	private Question question = new Question();
+	@JoinColumn(name = "test_id")
+	private Test test;
 	
-	@Size(min=1)
-	@Column(name = "data")
-	private String data;
+//	@NotNull
+//	@Column(name = "test_id")
+//	private Integer testId;
+
+	 @NotNull
+	 @OneToOne
+	 @JoinColumn(name = "question_id")
+	 private Question question = new Question();
 	
-	@Size(min=1)
-	@Column(name = "checked")
-	private String checked;
-	
+//	@NotNull
+//	@Column(name = "question_id")
+//	private Integer questionId;
+
+	// It will contain paragraph answer and MCQ related answer
+	@OneToMany(mappedBy = "answer", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<AnswerData> answerDataList = new ArrayList<AnswerData>();
+
 	@NotNull
+	@Column(name = "creator_id")
+	private Integer creatorId;
+
+	@Column(name = "last_editor_id")
+	private Integer lastEditorId;
+
+	@Column(name = "checked")
+	private Boolean checked;
+
 	@Column(name = "marks_obtained")
 	private Integer marksObtained;
-	
-	
+
+
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "creation_ts", updatable = false, nullable = false)
-	private Date creationTS; 
+	private Date creationTS;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "last_edited_ts")
 	private Date lastEditedTS;
+
+//	public Integer getTestId()
+//	{
+//		return test;
+//	}
+//
+//	public void setTestId(Integer testId)
+//	{
+//		this.test = testId;
+//	}
+
+//	public Integer getQuestionId()
+//	{
+//		return questionId;
+//	}
+//
+//	public void setQuestionId(Integer questionId)
+//	{
+//		this.questionId = questionId;
+//	}
+
+	public Test getTest()
+	{
+		return test;
+	}
+
+	public void setTest(Test test)
+	{
+		this.test = test;
+	}
+
+	public List<AnswerData> getAnswerDataList()
+	{
+		return answerDataList;
+	}
+
+	public void setAnswerDataList(List<AnswerData> answerDataList)
+	{
+		this.answerDataList = answerDataList;
+	}
+
+	public Integer getId()
+	{
+		return id;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+	 public Question getQuestion()
+	 {
+	 return question;
+	 }
 	
+	 public void setQuestion(Question question)
+	 {
+	 this.question = question;
+	 }
+
+	public Integer getCreatorId()
+	{
+		return creatorId;
+	}
+
+	public void setCreatorId(Integer creatorId)
+	{
+		this.creatorId = creatorId;
+	}
+
+	public Integer getLastEditorId()
+	{
+		return lastEditorId;
+	}
+
+	public void setLastEditorId(Integer lastEditorId)
+	{
+		this.lastEditorId = lastEditorId;
+	}
+
+	public Boolean getChecked()
+	{
+		return checked;
+	}
+
+	public void setChecked(Boolean checked)
+	{
+		this.checked = checked;
+	}
+
+	public Integer getMarksObtained()
+	{
+		return marksObtained;
+	}
+
+	public void setMarksObtained(Integer marksObtained)
+	{
+		this.marksObtained = marksObtained;
+	}
+
 	@Override
 	public Date getCreationTS()
 	{
